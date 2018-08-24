0f068a0d46f694084bd79cb5a9d8eba212b95536
BankNotes <sumirey@hubii-network.com>
2018-08-24 08:25:10 +0530

    BankNotes sourcecode

---
+++ repo/src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BankNotes-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BankNotesURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
